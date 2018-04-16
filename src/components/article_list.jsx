e2e02265728a2d35fbda5552cb1215a6c4defbc1
Jen <30688276+helplah@users.noreply.github.com>
2018-04-16 23:23:58 +0800

    first prototype

--- src/components/article_list.jsx
+++ src/components/article_list.jsx
@@ -3,11 +3,15 @@ import ArticleRow from "./article_row";
 
 const ArticleList = ({dataArray}) => {
   return (
-    <div className="container container--spacing container--font">
-      <div>
-        <span className="col-md-2 article__title text">Article title</span>
-        <span className="col-md-10 text">Snippet (click to view article)</span>
-      </div>
+    <div className="container container--margin--top container--font">
+      <table className="header__table--margin--btm">
+        <thead>
+          <tr>
+            <th className="text article__title">Article Title</th>
+            <th className="text snippet">Snippet (click to view article)</th>
+          </tr>
+        </thead>
+      </table>
       <ArticleRow dataArray={dataArray} />
     </div>
   );
