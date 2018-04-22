b9f6134672e6e734f98cfaf7fe7f807cd6f8d2c2
Jen <30688276+helplah@users.noreply.github.com>
2018-04-22 12:37:06 +0800

    first grid prototype

--- src/components/article_list.jsx
+++ src/components/article_list.jsx
@@ -4,8 +4,8 @@ import ArticleRow from "./article_row";
 const ArticleList = ({dataArray}) => {
   return (
     <section className="grid container container--margin container--font">
-        <h4 className="header-text article__title">Article Title</h4>
-        <h4 className="header-text snippet">Snippet (click to view article)</h4>
+      <h4 className="header-text article__title">Article Title</h4>
+      <h4 className="header-text snippet">Snippet (click to view article)</h4>
       <ArticleRow dataArray={dataArray} />
     </section>
   );
